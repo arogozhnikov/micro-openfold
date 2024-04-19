da45ed26dbac7b855f7cf97aa8a025c0274137df
Alex Rogozhnikov <iamfullofspam@gmail.com>
2024-04-19 10:45:33 -0700

    remove usages of openfold's kernel, raise warning when necessary.

--- openfold/model/primitives.py
+++ openfold/model/primitives.py
@@ -33,8 +33,7 @@ import torch.nn as nn
 from scipy.stats import truncnorm
 
 from openfold.utils.checkpointing import get_checkpoint_fn
-from openfold.utils.chunk_utils import _chunk_slice
-from openfold.utils.kernel.attention_core import attention_core
+from openfold.utils.precision_utils import is_fp16_enabled
 from openfold.utils.tensor_utils import (
     permute_final_dims,
     flatten_final_dims,
@@ -478,16 +477,19 @@ class Attention(nn.Module):
         # [*, H, Q/K, C_hidden]
         q, k, v = self._prep_qkv(q_x, kv_x)
 
-        # [*, Q, H, C_hidden]
-        if(use_memory_efficient_kernel):
-            if(len(biases) > 2):
+        if is_fp16_enabled():
+            use_memory_efficient_kernel = False
+        
+        if use_memory_efficient_kernel:
+            raise RuntimeError("memory-efficient kernel was deleted")
+        elif use_deepspeed_evo_attention:
+            if len(biases) > 2:
                 raise ValueError(
-                    "If use_memory_efficient_kernel is True, you may only "
+                    "If use_deepspeed_evo_attention is True, you may only "
                     "provide up to two bias terms"
                 )
-            o = attention_core(q, k, v, *((biases + [None] * 2)[:2]))
-            o = o.transpose(-2, -3)
-        elif(use_lma):
+            o = _deepspeed_evo_attn(q, k, v, biases)
+        elif use_lma:
             biases = [
                 b.expand(b.shape[:-2] + (q_x.shape[-2],) + (kv_x.shape[-2],)) 
                 for b in biases

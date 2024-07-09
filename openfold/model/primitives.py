ea1dc71cac308f88f61c5777b0750b007d5a3fdd
jacquesboitreaud <jacques@chaidiscovery.com>
2024-07-08 22:21:38 -0700

    fix

--- openfold/model/primitives.py
+++ openfold/model/primitives.py
@@ -33,7 +33,6 @@ import torch.nn as nn
 from scipy.stats import truncnorm
 
 from openfold.utils.checkpointing import get_checkpoint_fn
-from openfold.utils.precision_utils import is_fp16_enabled
 from openfold.utils.tensor_utils import (
     permute_final_dims,
     flatten_final_dims,
@@ -477,18 +476,9 @@ class Attention(nn.Module):
         # [*, H, Q/K, C_hidden]
         q, k, v = self._prep_qkv(q_x, kv_x)
 
-        if is_fp16_enabled():
-            use_memory_efficient_kernel = False
-        
+        use_memory_efficient_kernel = False
         if use_memory_efficient_kernel:
             raise RuntimeError("memory-efficient kernel was deleted")
-        elif use_deepspeed_evo_attention:
-            if len(biases) > 2:
-                raise ValueError(
-                    "If use_deepspeed_evo_attention is True, you may only "
-                    "provide up to two bias terms"
-                )
-            o = _deepspeed_evo_attn(q, k, v, biases)
         elif use_lma:
             biases = [
                 b.expand(b.shape[:-2] + (q_x.shape[-2],) + (kv_x.shape[-2],)) 
